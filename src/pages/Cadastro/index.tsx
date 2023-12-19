ebe10e8c4e7b9660cac80caf6465cd8b75de92ba
Aldo Santos <aldoacesso@gmail.com>
2023-12-19 15:32:47 -0300

    Criando tarefa

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,3 +1,12 @@
-const Cadastro = () => <div>nova tarefa</div>
+import BarraLateral from '../../conteiners/BarraLateral'
+import Formulario from '../../conteiners/Formulario'
+
+const Cadastro = () => (
+  <>
+    {/* barra lateral */}
+    <BarraLateral />
+    <Formulario />
+  </>
+)
 
 export default Cadastro

3d21ce267a97d57d808e591861a12b1411964449
Aldo Santos <aldoacesso@gmail.com>
2023-12-19 12:03:03 -0300

    Navegando entre rotas

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import BotaoAdicionar from '../../components/BotaoAdicionar'
+import BarraLateral from '../../conteiners/BarraLateral'
+import ListaDeTarefas from '../../conteiners/ListaDeTarefas'
+
+const Home = () => (
+  <>
+    <BarraLateral />
+    <ListaDeTarefas />
+    <BotaoAdicionar />
+  </>
+)
+
+export default Home
